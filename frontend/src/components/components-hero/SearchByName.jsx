e0319aebbaa47e02791b0241ef891d7eb3830de7
Yuganshu <programmingenjoyer98@gmail.com>
2024-05-29 13:35:55 +0530

    Hit enter to search added

--- frontend/src/components/components-hero/SearchByName.jsx
+++ frontend/src/components/components-hero/SearchByName.jsx
@@ -10,6 +10,22 @@ export default function SearchByName(props) {
         setName(newPokemonName);
     }
 
+    try {
+        const input = document.querySelector('#search-by-name input');
+        const searchButton = document.querySelector('#search-by-name button[title="Search"]');
+
+        input.addEventListener('keydown', function (event) {
+            // Check if the key pressed is 'Enter' (key code 13)
+            if (event.key === 'Enter') {
+                // Trigger a click event on the search button
+                searchButton.click();
+            }
+        });
+    } catch (error) {
+        console.log(`Waiting for input and search buttons to load`);
+    }
+
+
     return (
 
         <div className='relative mb-8' id='search-by-name'>
@@ -23,7 +39,7 @@ export default function SearchByName(props) {
                     className="w-full text-center p-2 rounded bg-yellow-400 text-black placeholder-gray-700 focus:bg-[#ffd966] focus:outline-none"
                 />
                 <button title='Search' onClick={() => { props.receiveData("pokemonName", name); props.setPageNum(1); setName(""); props.scrollToCardSection(); }} className="absolute right-2 top-2">
-                    <img src={getImageURL("search.png")} alt="" className='w-6 h-6' />
+                    <img src={getImageURL("search.png")} alt="Search" className='w-6 h-6' />
                 </button>
             </div>
         </div>
